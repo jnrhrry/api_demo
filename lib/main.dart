b5aa49beda3e2557ec866260bd31abe5cd301485
Harry Januar <januarjanuarharry@gmail.com>
2020-09-22 07:06:21 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+import 'post_result_model.dart';
+import 'user_model.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  PostResult postResult = null;
+  User user = null;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("API Demo"),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Text((user != null)
+                  ? user.id + " | " + user.name
+                  : "Tidak ada data"),
+              RaisedButton(
+                onPressed: () {
+                  User.connecToAPI("3").then((value) {
+                    user = value;
+                    setState(() {});
+                  });
+                },
+                child: Text("GET"),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
